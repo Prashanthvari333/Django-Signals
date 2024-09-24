8dd1197c8d3a572ee3b6e8bd17da40051487bf3f
Prashanthvari333 <prashanthvarikuppala333@gmail.com>
2024-09-24 12:24:09 +0530

    url changed for user delete

--- signals/app/urls.py
+++ signals/app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('index', views.index,name='index'),
     path('profile',views.create_user_profile,name='create_user_profile'),
     path('books',views.create_book,name='books'),
-    path('delete_profile',views.delete_user,name='delete_user_profile'),
+    path('delete_user',views.delete_user,name='delete_user'),
 ]
